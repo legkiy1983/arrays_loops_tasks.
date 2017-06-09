c67f954bbc7d8650d9e6659366112277e0311eec
legkiy1983 <vovik-l@yandex.ru>
2017-06-09 19:58:40 +0300

    Update 14.php

--- 14.php
+++ 14.php
@@ -1,10 +1,11 @@
 <?php
-
-$arr=array(4, 2, 5, 19, 13, 0, 10);
-foreach($arr as $ar){
-    if ($ar>1 and $ar<5) {
-        echo $ar." -Есть!";
-    }else{
-        echo "{$ar}- Nety!";
+$arr = array (4, 2, 5, 19, 13, 0, 10);
+foreach ($arr as $var) {
+    if ($var == 2 || $var == 3 || $var == 4) {
+        echo 'Есть!';
+        break;
+    } else {
+        echo 'Нет!';
+        break;
     }
 }
