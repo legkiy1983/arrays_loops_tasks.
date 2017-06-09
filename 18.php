0c4385f159c490942261a2c36c31a95614fa8b61
legkiy1983 <vovik-l@yandex.ru>
2017-06-09 19:54:43 +0300

    Update 18.php

--- 18.php
+++ 18.php
@@ -1,10 +1,12 @@
 <?php
 
-$arr=array(1,2,3,4,5,6,7);
-foreach ($arr as $ar){
-        if ($ar==6 or $ar==7){
-            echo "<b>$ar</b></br>";
-        }else {
-           echo $ar."<br>";
-        }
+$arr = array('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+foreach ($arr as $var) {
+    if ($var == 'Суббота') {
+        echo "<b>$var</b>" . ' ';
+    } elseif ($var == 'Воскресенье') {
+        echo "<b>$var</b>";
+    }else {
+        echo $var . ' ';
+    }
 }
